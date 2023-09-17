4a35462267c467965603b012310195813b3ba229
suriyanS <s.suriyanmca@gmail.com>
2023-09-17 21:31:00 +1000

    Add StackBlitz fix

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'zone.js/dist/zone';
 import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
